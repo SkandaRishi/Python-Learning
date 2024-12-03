dacb8d4a1329066f25f3fdeef676365534ed62d6
SkandaRishi <1247ankit@gmail.com>
2024-12-03 09:33:32 +0530

    statements in python

---
+++ Youtube/Python_with_CWH/Introduction/statement.py
@@ -0,0 +1,8 @@
+# use this hastag to comment this, this is comment
+print("Hey I am a good boy\nand this viewr is also a good boy/girl ")   
+# use \n to print in next line
+# or u can simply type ctrl+ forward slash to comment or uncomment any line
+'''
+this triple apostrophe or double quote """ symbol use for commenting 
+multiple lines
+'''
